ddc386866d5406621003f70990e6740a9e0ac32a
deigos339 <diegoromerocalderon@gmail.com>
2020-08-28 23:52:45 -0500

    barras de texto, boton e implementado hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
+  const [stateCity, setStateCity] = useState("");
+  const [stateDate, setStateDate] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <div className="row">
+            <div id="city" className="col">
+              <input 
+               type="text"
+               onChange={(e)=>{setStateCity(e.target.value)}}
+               value={stateCity} 
+               placeholder="City"
+               />
+            </div>
+            <div id="date" className="col">
+              <input
+               type="text"
+               onChange={(e)=>{setStateDate(e.target.value)}}
+               value={stateDate}
+               placeholder="Date"
+               />
+            </div>
+          </div>
+        </div>
+        <div className="col mt-2">
+          <button 
+          type="button" 
+          class="btn btn-info btn-lg "
+          onClick=""
+          >
+          Info
+          </button>
+        </div>
       </header>
     </div>
   );
